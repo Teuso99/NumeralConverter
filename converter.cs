25401aa076102cb53068dc6368c2b49a21f9530b
Mateus Machado <mateusmmachado2@gmail.com>
2020-01-16 17:24:32 -0300

    added BINtoDEC method

--- converter.cs
+++ converter.cs
@@ -6,11 +6,11 @@ namespace NumeralConverter
     {
         public static void BinarytoDecimal(string number)
         {
-            int result, y = 0;
+            int result = 0 , y = 0;
 
-            for (int x = sizeof(number) - 1; x == 0; x--)
+            for (int x = number.Length - 1; x == 0; x--)
             {
-                result += (int)number[x] * Math.Pow(y, 2);
+                result += (int)number[x] * (int)Math.Pow(y, 2);
                 y++;
             }
 
@@ -19,7 +19,33 @@ namespace NumeralConverter
 
         public static void DecimaltoBinary(string number)
         {
-            
+            /*
+            int aux, y = 0, result;
+            bool greaterThan = true;
+            //string binary;
+
+            aux = Int32.Parse(number);
+
+            //implementar métodos/recursão
+            while(greaterThan)
+            {
+                if(aux > Math.Pow(2,y))
+                {
+                    y++;
+                }
+                else if(aux == Math.Pow(2,y))
+                {
+                    result = (int)Math.Pow(10, y);
+                    greaterThan = false;
+                }
+                else if(aux < Math.Pow(2,y))
+                {
+                    aux = (int)Math.Pow(2,y) - aux;
+                    //binary = " ";
+                    y--;
+                }
+            }
+            */
         }
 
         public static void HextoDecimal(string number)
